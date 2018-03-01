6426ab3404ee0753de4f295edd0c2ed859111be1
soleilxie <36553435+soleilxie@users.noreply.github.com>
2018-03-01 11:22:16 -0800

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -29,3 +29,35 @@ def Fac(a):
             result = i*result
             i += 1
         return result 
+choice = 0
+while choice != 7:
+    print()
+    print("1. Add 2 Numbers")
+    print("2. Subtract 2 Numbers")
+    print("3. Multiply 2 Numbers")
+    print("4. Divide 2 Numbers")
+    print("5. Exponent of 1 Number")
+    print("6. Factorial of 1 Number")
+    print("7. Exit")
+    print()
+    
+    choice = int(input("Your Choice: "))
+    if choice < 5:
+        a = int(input("First Number: "))
+        b = int(input("Second Number: "))
+        if choice == 1:
+            print("Answer: " + str(Add(a,b)))
+        elif choice == 2:
+            print("Answer: " + str(Sub(a,b)))
+        elif choice == 3:
+            print("Answer: " + str(Mul(a,b)))
+        elif choice == 4:
+            print("Answer: " + str(Div(a,b)))
+    elif choice == 5:
+        a = int(input("Base: "))
+        b = int(input("Exponent: "))
+        print("Answer: " + str(Exp(a,b)))
+    elif choice == 6:
+        a = int(input("Number: "))
+        print("Answer: " + str(Fac(a)))
+
